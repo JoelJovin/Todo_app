747a2ac8d738855416a13f5b8b41c3acd772d946
JoelJovin <joel.premkumar1@freshworks.com>
2024-06-11 14:15:17 +0530

    Zod Validations done

--- index.js
+++ index.js
@@ -5,23 +5,39 @@
 const express = require ("express");
 //Using the imported express library
 const app = express();
+const { createTodo } = require("./types");
+
 
 //To log the error where occured in the whole app
 app.use(express.json());
 
 //Creating POST method to create a todo
-app.post("todo", function(req, res){
-
+app.post("/todos", function(req, res){
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if (!parsePayload.success){
+        res.status(411).json({ 
+            msg: "Please check your inputs",
+        })
+        return;
+    }
+    
 })
 
 //Creating a GET method to read the todo which created
-app.get("todo", function (req, res){
+app.get("todos", function (req, res){
 
 })
 
 //Creating a PUT method to Update or Edit the todo
-app.put("todo", function(req, res){
+app.put("/completed", function(req, res){
+    const createId = req.body;
+    if (!createId.success){
+        res.status(411).json({
+            msg: "Please recheck the Input you've entered",
+        })
+        return;
+    }
+    
 
 })
-
-//Commit aagirukka nu check pannu
